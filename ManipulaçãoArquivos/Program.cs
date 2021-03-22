6ca17be26deed06c7646801cc125b68ced37a5ce
dkingfaria <diegosantosfaria@hotmail.com>
2021-03-21 23:18:57 -0300

    Add project files.

---
+++ ManipulaçãoArquivos/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManipulaçãoArquivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  StreamWriter escritor = new StreamWriter("teste.txt");
+            StreamWriter escritor = File.AppendText("teste.txt");
+            escritor.WriteLine("Udemy");
+            escritor.WriteLine("Teste de escrita");
+            escritor.Close();
+            */
+
+            StreamReader leitor = new StreamReader("teste.txt");
+            // string conteudo = leitor.ReadToEnd();
+            // Console.WriteLine(leitor.ReadLine());
+
+            List<string> linhas = new List<string>();
+            string linha = "";
+            while (linha != null)
+            {
+                linha = leitor.ReadLine();
+                if (linha != null)
+                {
+                    linhas.Add(linha);
+                }
+            }
+
+            leitor.Close();
+
+           // Console.WriteLine(conteudo);
+          // foreach(string nome in linhas)
+          //  {
+          //      Console.WriteLine(nome);
+          // }
+
+            Console.WriteLine("Arquivo gerado.");
+            Console.ReadLine();
+        }
+    }
+}
